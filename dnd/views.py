a5fce1ba8835b3000b3d6393c54b521a8156e9be
Luka Courmont <lcourmont@mdweb.fr>
2024-04-26 11:24:21 +0200

    login et singup qui marche

--- dnd/views.py
+++ dnd/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
     return HttpResponse('Hello, World!')
 
 def authView(request):
-    form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration/signup.html', {'form': form})
