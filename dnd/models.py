4aadc92b0bd8541f019eaec4b66d33f697ac1381
Luka Courmont <lcourmont@mdweb.fr>
2024-04-26 11:43:59 +0200

    migration skills

--- dnd/models.py
+++ dnd/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Skill(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username
