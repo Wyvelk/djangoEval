5f23964b0c006345277f7f6d665082aee6925f40
Michael <gaelle.balois@gmail.com>
2024-04-26 11:27:25 +0200

    commit

--- dnd/views.py
+++ dnd/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 from django.template import loader
+
 
 # Create your views here.
 def home(request):
@@ -9,5 +11,11 @@ def home(request):
     return HttpResponse(template.render())
 
 def authView(request):
-    form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration/signup.html', {'form': form})
