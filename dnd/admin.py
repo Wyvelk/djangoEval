114093f29017f5f40307ac4af807410249fd157e
Luka Courmont <lcourmont@mdweb.fr>
2024-04-30 10:45:36 +0200

    admin

--- dnd/admin.py
+++ dnd/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Skill, DiceRoll, Wallet
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('user', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma')
+
+admin.site.register(Skill, SkillAdmin)
+
+
+class DiceRollAdmin(admin.ModelAdmin):
+    list_display = ('user', 'skill', 'dice_roll', 'win')
+
+admin.site.register(DiceRoll, DiceRollAdmin)
+
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gold')
+
+admin.site.register(Wallet, WalletAdmin)
